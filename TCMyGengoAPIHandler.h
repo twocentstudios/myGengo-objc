4a3cd4bfd3be36ff4fdc889b2960d1f89e5486fc
Christopher Trott <chris@twocentstudios.com>
2012-06-14 22:39:17 -0500

    Init method.

--- TCMyGengoAPIHandler.h
+++ TCMyGengoAPIHandler.h
@@ -13,7 +13,9 @@
 
 #define API_HOST @"api.mygengo.com"
 #define SANDBOX_API_HOST @"api.sandbox.mygengo.com"
-#define VERSION @"0.1"
+#define API_VERSION @"1.1"
+#define WRAPPER_VERSION @"0.1"
+#define USER_AGENT @"myGengo ObjC Library; Version %@; http://mygengo.com/;"
 
 // Classes that use the API Handler should implement the delegate protocol
 // in order to receive feedback as to the status of their requests and
--- TCMyGengoAPIHandler.m
+++ TCMyGengoAPIHandler.m
@@ -11,4 +11,28 @@
 @implementation TCMyGengoAPIHandler
 
 
+- (id)initWithPublicKey:(NSString*)publicKey 
+             privateKey:(NSString*)privateKey
+            isSandboxed:(BOOL)isSandboxed{
+  
+  if ((self = [super init])){
+    _publicKey = [publicKey copy];
+    _privateKey = [privateKey copy];
+    _apiVersion = [NSString stringWithString:API_VERSION];
+    _debug = NO;
+    _userAgent = [NSString stringWithFormat:USER_AGENT, WRAPPER_VERSION];
+    _sandboxed = isSandboxed;
+    _apiHost = _sandboxed ? [NSString stringWithString:SANDBOX_API_HOST] : [NSString stringWithString:API_HOST];
+  }
+  
+}
+
+- (void)dealloc{
+  [_publicKey release]; _publicKey = nil;
+  [_privateKey release]; _privateKey = nil;
+  [_apiVersion release]; _apiVersion = nil;
+  [_userAgent release]; _userAgent = nil;
+  [_apiHost release]; _apiHost = nil;
+  [super dealloc];
+}
 @end
